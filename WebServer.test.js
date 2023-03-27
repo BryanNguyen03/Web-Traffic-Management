0a13e03539345df4625f03da79939d09663e8504
Trang Tran <89760594+Tr4ng-T@users.noreply.github.com>
2023-03-27 10:27:39 -0400

    creating testing

---
+++ WebServer.test.js
@@ -0,0 +1,18 @@
+const tempServer = require("../WebServer");
+const assert = require( "assert" );
+
+describe( "Server capacity", function () {
+  
+});
+
+describe( "Web Page", function () {
+  
+});
+
+describe( "Login", function () {
+  
+});
+
+describe( "Visitor", function () {
+  
+});
